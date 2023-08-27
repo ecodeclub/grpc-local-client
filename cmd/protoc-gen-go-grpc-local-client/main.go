9fd5b2d6699d803d8630f09ce7a574c554aab452
Deng Ming <ming_flycash@qq.com>
2023-08-27 13:42:58 +0800

    第一个版本

---
+++ cmd/protoc-gen-go-grpc-local-client/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/ecodeclub/grpc-local-client/internal/genclient"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func main() {
+	protogen.Options{}.Run(func(plugin *protogen.Plugin) error {
+		for _, f := range plugin.Files {
+			g := genclient.NewGenerator(plugin, f)
+			err := g.Generate()
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
